a52fcc1f7c482e4d50564c82a3d1ea605db1f946
Dagson Gabriel <dagson.g.01@gmail.com>
2024-03-16 22:17:41 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid'
+
+const LOCAL_STORAGE_KEY_TODOLIST = 'todoList';
+
+function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    setTodoList(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_TODOLIST)));
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_TODOLIST, JSON.stringify(todoList));
+  }, [todoList])
+
+  function handleAddTodo(event){
+    setTodoList(prevTodoList => [...prevTodoList, {id: uuidv4(), name: name, complete: false}])
+    setName('');
+  }
+
+  function toggleTodo(id){
+    const newTodoList = [...todoList];
+    const todo = newTodoList.find(x => x.id === id);
+    todo.complete = !todo.complete;
+
+    setTodoList(newTodoList)
+  }
+
+  const handleChangeName = (event) => setName(event.target.value);
+
+  function handleClean(){
+    const todoNotComplete = todoList.filter(x => !x.complete);
+    setTodoList(todoNotComplete);
+  }
+
+  return (
+    <div className='container'>
+      <div className='controls'>
+        <input value={name} type='text' onChange={handleChangeName}/>
+        <button onClick={handleAddTodo}>Add ToDo</button>
+        <button onClick={handleClean} className='clear-btn'>Limpar</button>
+      </div>
+      <div className='todo-list-container'>
+        <TodoList todoList={todoList} toggleTodo={toggleTodo}/>
+      </div>
+      <p>{todoList.filter(x => x.complete).length} Tarefa(s) Concluídas</p><br/>
+      <p>{todoList.filter(x => !x.complete).length} Tarefa(s) Não Concluídas</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Todo.css'
+
+export default function Todo({todo, toggleTodo}){
+  function handleChangeTodo(){
+    toggleTodo(todo.id);
+  }
+  
+  return(
+    <label>
+      <input type="checkbox" onChange={handleChangeTodo} checked={todo.complete}/>
+      <p className={todo.complete ? 'complete' : ''}>{todo.name}</p><br/>
+    </label> 
+  )
+}
